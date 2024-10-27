93f278e7329e3b52f53cccfbe2fff2a67b79bfd9
Lazy <rain121212121212@gmail.com>
2024-10-27 18:17:20 +0800

    5.2.5 Mobile Menu Update

--- vehicle/js/myAccount.js
+++ vehicle/js/myAccount.js
@@ -1,10 +1,24 @@
+$(document).ready(function () {
+    $('.menu-icon-close').hide();
+    $('.menu-icon-open').click(function () {
+        $('.mobile-menu').show();
+        $('.menu-icon-open').hide();
+        $('.menu-icon-close').show();
+    });
+    $('.menu-icon-close').click(function () {
+        $('.mobile-menu').hide();
+        $('.menu-icon-close').hide();
+        $('.menu-icon-open').show();
+    });
+});
+
 // Tab switching functionality
 document.querySelectorAll('.tab').forEach(tab => {
     tab.addEventListener('click', () => {
         // Remove active class from all tabs
         document.querySelectorAll('.tab').forEach(t => t.classList.remove('active'));
         document.querySelectorAll('.tab-pane').forEach(p => p.classList.remove('active'));
-        
+
         // Add active class to clicked tab
         tab.classList.add('active');
         document.getElementById(tab.dataset.tab).classList.add('active');
--- vehicle/js/vehicles.js
+++ vehicle/js/vehicles.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     // Wishlist functionality
-    $('.wishlist-btn').on('click', function () {
+    $(".wishlist-btn").on("click", function () {
         if ($(this).text() === "❤️") {
             $(this).text("♡");
         } else {
@@ -8,16 +8,28 @@ $(document).ready(function () {
         }
     });
 
+    $(".menu-icon-close").hide();
+    $(".menu-icon-open").click(function () {
+        $(".mobile-menu").show();
+        $(".menu-icon-open").hide();
+        $(".menu-icon-close").show();
+    });
+    $(".menu-icon-close").click(function () {
+        $(".mobile-menu").hide();
+        $(".menu-icon-close").hide();
+        $(".menu-icon-open").show();
+    });
+
     // Filter functionality
-    $('select').on('change', function () {
+    $("select").on("change", function () {
         // Here you would typically make an API call to filter results
         console.log("Filter changed:", this.id, this.value);
     });
 
     // Pagination functionality
-    $('.page-btn').on('click', function () {
-        $('.page-btn.active').removeClass('active');
-        $(this).addClass('active');
+    $(".page-btn").on("click", function () {
+        $(".page-btn.active").removeClass("active");
+        $(this).addClass("active");
         // Here you would typically make an API call to get the next page of results
     });
 });
