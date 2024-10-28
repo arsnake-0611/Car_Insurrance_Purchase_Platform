c73b1aff2c44b051a4cba727eec6030ba58714ff
Lazy <rain121212121212@gmail.com>
2024-10-28 22:15:39 +0800

    5.2.9.1 Wishlist Request Quote left

--- vehicle/js/wishlist.js
+++ vehicle/js/wishlist.js
@@ -1,5 +1,6 @@
-$(document).ready(function() {
-    $('.btn-remove').on('click', function() {
+$(document).ready(function () {
+    $('.btn-remove').on('click', function () {
+        if (confirm("Are you sure you want to remove this item from your wishlist?")) {   
         const wishlistItem = $(this).closest('.wishlist-item');
         wishlistItem.css('opacity', '0');
         setTimeout(() => {
@@ -7,29 +8,31 @@ $(document).ready(function() {
             const remainingItems = $('.wishlist-item');
             if (remainingItems.length === 0) {
                 const emptyState = $('<div>')
-                   .addClass('empty-wishlist')
-                   .html(`
-                        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor">
-                            <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
-                        </svg>
+                    .addClass('empty-wishlist')
+                    .html(`
+                        <img src="./photo/empty.png" style="width:200px;height:200px;" alt="Empty Image">
                         <h2>Your wishlist is empty</h2>
-                        <p>Start adding vehicles to your wishlist to keep track of your favorites</p>
-                        <a href="https://legendmotor.com/vehicles" class="btn btn-primary">Browse Vehicles</a>
+                        <p style="margin-bottom:15px;">Start adding vehicles to your wishlist to keep track of your favorites</p>
+                        <a href="vehicles.html"><button class="btn btn-primary">Browse Vehicles</button></a>
                     `);
                 $('.wishlist-container').append(emptyState);
             }
         }, 300);
+        }
+        else {
+            return false;
+        }
     });
-   
+
+    $('.menu-icon-close').hide();
+    $('.menu-icon-open').click(function () {
+        $('.mobile-menu').show();
+        $('.menu-icon-open').hide();
+        $('.menu-icon-close').show();
+    });
+    $('.menu-icon-close').click(function () {
+        $('.mobile-menu').hide();
         $('.menu-icon-close').hide();
-                $('.menu-icon-open').click(function () {
-                    $('.mobile-menu').show();
-                    $('.menu-icon-open').hide();
-                    $('.menu-icon-close').show();
-                });
-                $('.menu-icon-close').click(function () {
-                    $('.mobile-menu').hide();
-                    $('.menu-icon-close').hide();
-                    $('.menu-icon-open').show();
-                });
+        $('.menu-icon-open').show();
+    });
 });
