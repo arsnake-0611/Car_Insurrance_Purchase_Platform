4e261e23881737a673dcd0b395f8d58c7bdcd698
arsnake-0611 <167392109+arsnake-0611@users.noreply.github.com>
2024-11-01 00:56:20 +0800

    5.7.2 Fixed the problem of info page
    NOT YET MAKE THE TEXT OF THE GUIDED

--- quotation/info.js
+++ quotation/info.js
@@ -1,32 +1,30 @@
 $(document).ready(function() {
-  $('a[href^="#"]').on('click', function(event) {
-      var target = $(this.hash);
-      if (target.length) {
-          event.preventDefault();
-          $('html, body').animate({
-              scrollTop: target.offset().top
-          }, 500);
-      }
-  });
+    // Hide text class elements when page loads
+    $('.text').hide();
 
-  $(window).scroll(function() {
-      $('.feature-card').each(function() {
-          let position = $(this).offset().top;
-          let scroll = $(window).scrollTop();
-          let windowHeight = $(window).height();
-          
-          if (scroll > position - windowHeight + 200) {
-              $(this).addClass('animate');
-          }
-      });
-  });
+    $('.cta-button').hover(
+        function() {
+            $(this).css('background-color', '#ffd700');
+        },
+        function() {
+            $(this).css('background-color', '#ffcc00');
+        }
+    );
 
-  $('.cta-button').hover(
-      function() {
-          $(this).css('background-color', '#ffd700');
-      },
-      function() {
-          $(this).css('background-color', '#ffcc00');
-      }
-  );
+    $('.feature-card').each(function() {
+        $(this).find('.feature-icon').hide();
+    });
+
+    $('.feature-card').hover(
+        function() {
+            $(this).css('transform', 'translateY(-10px)')
+                   .css('box-shadow', '5px 5px 5px #ffe26e');
+            $(this).find('.text').fadeIn(); // Show text on hover
+        },
+        function() {
+            $(this).css('transform', 'translateY(0)')
+                   .css('box-shadow', '5px 5px 5px #6e8bff');
+            $(this).find('.text').fadeOut(); // Hide text when hover ends
+        }
+    );
 });
